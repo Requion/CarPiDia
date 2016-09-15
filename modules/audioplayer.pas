7caa81fa9a1e820a3caa4a0a075d2170f656597c
Requion <Requion@users.noreply.github.com>
2016-09-15 11:20:35 +0200

    added skeletons for modules

---
+++ modules/audioplayer.pas
@@ -0,0 +1,13 @@
+unit audioplayer;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+implementation
+
+end.
+
---
+++ modules/diag.pas
@@ -0,0 +1,13 @@
+unit diag;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+implementation
+
+end.
+
